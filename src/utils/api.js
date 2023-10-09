ee0bb46daa7eeee451c34ac221fdb2f8f6538da8
samyak-max <samyak_c@bt.iitr.ac.in>
2023-10-09 19:03:13 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const API_URL = "https://api.quicksell.co/v1/internal/frontend-assignment";
 
-export const getTickets = () => {
-  return axios.get(API_URL).then((response) => response.data.tickets);
+export const getTickets = async () => {
+  const response = await axios.get(API_URL);
+  return response.data.tickets;
 };
 
-export const getUsers = () => {
-  return axios.get(API_URL).then((response) => response.data.users);
+export const getUsers = async () => {
+  const response = await axios.get(API_URL);
+  return response.data.users;
 };
